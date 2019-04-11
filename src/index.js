9ba05fc1043836e2e7090595358ed15ee44589e3
cameoapp <benjamin.eisner.20@dartmouth.edu>
2019-04-11 12:55:49 -0400

    version 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("starting up!");
+import $ from 'jquery';
+import './style.scss';
+
+class PageTimer {
+  constructor(start) {
+    this.elapsedTime = start;
+  }
+
+  startTimer() {
+    setInterval(() => {
+      this.displayTime();
+      this.elapsedTime += 1;
+    }, 1000);
+  }
+
+  displayTime() {
+    $('#main').html(
+      `You've been on this page for ${this.elapsedTime} seconds.`,
+    );
+  }
+}
+
+const pageTimer = new PageTimer(0);
+pageTimer.startTimer();
